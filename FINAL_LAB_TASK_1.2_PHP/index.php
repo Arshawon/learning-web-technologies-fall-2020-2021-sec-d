7aa0cf0ccb1b1803605a25bade58b44f2f453a64
Asif reza <arshawon73@gmail.com>
2020-12-02 08:46:02 +0600

    Lab task

---
+++ FINAL_LAB_TASK_1.2_PHP/index.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html >
+<head>
+
+    <title>index</title>
+</head>
+<body>
+    
+    <center>
+           
+        <h1>Shop Management System</h1>
+        
+        <form action="  " method="post">
+        
+        <input type="text" name="username" ><br><br>
+        <input type="password" name="password" ><br><br>
+        <input type="submit" name="submit" value="Submit">
+        <input type="button" name="signup" value="Sign Up"></a>
+        
+    </form>
+        
+    </center>
+    
+</body>
+</html>
---
+++ FINAL_LAB_TASK_1.2_PHP/reg.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html >
+<head>
+    <title>Reg</title>
+</head>
+<body>
+        
+    <center>
+         
+            <legend><Reg></legend>
+            
+            <form action=" " method="post">
+            
+            Name : <input type="text" name="name"><br><br>
+            Contact No : <input type="text" name="contactNo"><br><br>
+            UserName : <input type="text" name="username"><br><br>
+            password : <input type="password" name="password"><br><br>
+            <input type="submit" name="submit" value="Submit">
+        </form>
+            
+ 
+    </center>
+    
+</body>
+</html>
